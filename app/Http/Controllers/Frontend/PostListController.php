f9a3d0c0e9fa44476c3f8db831d04f8ae2b0bd13
MMMoroz <nik.rachok1995@mail.ru>
2025-02-28 16:11:39 +1000

    Initial commit

---
+++ app/Http/Controllers/Frontend/PostListController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostListController extends Controller
+{
+    public function index()
+    {
+//        $posts = Posts::simplePaginate(5);
+////        $categories = Category::all();
+        $posts = Post::with('tags')->addSelect(
+            [
+                'category_name' => Category::select('title')
+                    ->whereColumn('category_id', 'categories.id'),
+                'category_slug' => Category::select('slug')
+                    ->whereColumn('category_id', 'categories.id'),
+                'tags' => Category::select('slug')
+                    ->whereColumn('category_id', 'categories.id'),
+            ]
+        )->latest()->paginate(15);
+        return view('frontend.posts', compact('posts'));
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $articles = Post::latest()->paginate(15);
+        return view('frontend.blog',compact('articles'));
+    }
+
+}
